9397af4f3785f591ea4503adb63f4260440a6be6
wayne54515 <0920005wayne@gmail.com>
2019-05-10 23:59:00 +0800

    創建需要的Model

---
+++ app/Models/Food.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends BaseModel
+{
+    //example
+    // protected $fillable = [
+    //     'id', 'name', 'attribute'
+    // ];
+
+    // /**
+    //  * 取得  模型
+    //  *
+    //  * @return App\Models\
+    //  */
+    // public function (){
+    //     return $this->hasOne('App\Models\', 'id', '_id');
+    // }
+
+    // /**
+    //  * 取得  模型
+    //  * 
+    //  * @return App\Models\
+    //  */
+    // public function (){
+    //     return $this->hasMany('App\Models\', '_id', 'id');
+    // }
+}
