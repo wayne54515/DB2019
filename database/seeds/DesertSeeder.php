fccf0cf4d6fc4b23394a09815be023bad2ae0839
wayne54515 <0920005wayne@gmail.com>
2019-05-12 14:54:22 +0800

    增加Seeder檔

---
+++ database/seeds/DesertSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DesertSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
